477c2f468d25161081a73e0d69d79c11ae1a2e0d
SooyeonCheon123 <suyeon5445@gmail.com>
2023-02-15 14:23:13 -0800

    input file created

--- play.js
+++ play.js
@@ -1,26 +1,7 @@
-const net = require("net");
-const connect = require('./client');
-
+const { connect } = require('./client');
+const { setupInput } = require('./input');
 
 console.log("Connecting ...");
 connect();
-
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.on("data", handleUserInput);
-  stdin.resume();
-  return stdin;
-};
-
 
 setupInput();
