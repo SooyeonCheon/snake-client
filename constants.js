b27be04eb0c026316f258d1ee9a4fd7e90bd0521
SooyeonCheon123 <suyeon5445@gmail.com>
2023-02-15 15:54:35 -0800

    add messages and constants

---
+++ constants.js
@@ -0,0 +1,22 @@
+
+const IP = '165.227.47.243';
+const PORT = 50541;
+const MOVE_UP_KEY = 'w';
+const MOVE_LEFT_KEY = 'a';
+const MOVE_DOWN_KEY = 's';
+const MOVE_RIGHT_KEY = 'd';
+const MESSAGES = {
+  1 : 'Say: Hello!',
+  2 : 'Say: Nice to meet you',
+  3 : 'Say: How are you'
+}
+
+module.exports = {
+  IP,
+  PORT,
+  MOVE_UP_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_RIGHT_KEY,
+  MESSAGES
+};
