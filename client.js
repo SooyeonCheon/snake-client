bec14743cce31fc18d665d74fe84085635ad9446
SooyeonCheon123 <suyeon5445@gmail.com>
2023-02-15 13:38:35 -0800

    name information send to server

--- client.js
+++ client.js
@@ -1,14 +1,20 @@
+const net = require('net');
+
 const connect = function () {
   const conn = net.createConnection({
     host: '165.227.47.243',
     port: 50541
   });
   
-  conn.on("data", (data) => {
-    console.log("data: ___", data);
-  })
+  conn.setEncoding('utf8');
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+    conn.write('Name: CSY');
+  });
 
-  conn.setEncoding("utf8");
+  conn.on('data', (data) => {
+    console.log('data: ', data);
+  })
 
   return conn;
 };
--- play.js
+++ play.js
@@ -7,9 +7,3 @@ const connect = require('./client');
 console.log("Connecting ...");
 connect();
 
-/*
-connect.on("connect", (data) => {
-  // code that does something when the connection is first established
-  console.log(data);
-});
-*/
